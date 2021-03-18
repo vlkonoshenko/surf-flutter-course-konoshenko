bc2fd5c6a98b301dc962d0290fae03a8d3970360
Vlad Konoshenko <konoshenko.vl@gmail.com>
2021-03-19 00:20:19 +0300

    Add list_screen with Scaffold

---
+++ places/lib/ui/screen/sight_list_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SightListScreen extends StatefulWidget {
+  @override
+  _SightListScreenState createState() => _SightListScreenState();
+}
+
+class _SightListScreenState extends State<SightListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(16),
+        child: const TextField(),
+      ),
+    );
+  }
+}
