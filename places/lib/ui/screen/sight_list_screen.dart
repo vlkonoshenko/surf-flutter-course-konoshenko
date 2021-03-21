f379d7fb34a55bae85ac0054335ebb6a9dbba00c
Vlad Konoshenko <konoshenko.vl@gmail.com>
2021-03-21 21:06:37 +0300

    4.4 Text and TextStyle. Part 1

--- places/lib/ui/screen/sight_list_screen.dart
+++ places/lib/ui/screen/sight_list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SightListScreen extends StatefulWidget {
@@ -6,14 +7,38 @@ class SightListScreen extends StatefulWidget {
 }
 
 class _SightListScreenState extends State<SightListScreen> {
+  final String _title = 'Список\nинтересных мест';
+  final TextStyle _titleStyle = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontStyle: FontStyle.normal,
+    fontSize: 32,
+    color: Colors.black,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        bottom: PreferredSize(
+          preferredSize: Size(double.infinity, 72),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              _title,
+              style: _titleStyle,
+              textAlign: TextAlign.start,
+            ),
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
       body: Container(
         alignment: Alignment.center,
-        padding: const EdgeInsets.all(16),
-        child: const TextField(),
+        padding: EdgeInsets.all(16),
+        child: Container(),
       ),
     );
   }
